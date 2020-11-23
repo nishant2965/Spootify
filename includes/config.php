4d0449cea46b42445bd5655dc6f7a069b2403911
nishant2965 <43739140+nishant2965@users.noreply.github.com>
2020-11-23 07:48:36 +0530

    updated for mysql to connect to heroku

--- includes/config.php
+++ includes/config.php
@@ -4,7 +4,7 @@
 
 	$timezone = date_default_timezone_set("Europe/London");
 
-	$con = mysqli_connect("localhost", "root", "", "spootify");
+	$con = mysqli_connect("aqx5w9yc5brambgl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", "p0atdomnxw50mly4", "s8xiltczoxfef3in", "ep45tczw1gvqi8v9");
 
 	if(mysqli_connect_errno()) {
 		echo "Failed to connect: " . mysqli_connect_errno();
